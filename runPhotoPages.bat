8d1342b852094c3bc8a589d9cb8b75a140aaf790
Jerry Falkman <thefalk305@hotmail.com>
2025-08-06 11:26:34 -0700

    Remove deleted files

--- runPhotoPages.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Running Excel to JSON.py...
-python py/Excel_to_JSON.py
-
-echo Running buildPhotoPagesData.js...
-node js/buildPhotoPagesData.js
-
-echo ✅ All tasks completed.
-pause 
